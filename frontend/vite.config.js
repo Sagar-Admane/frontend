1ff763a8cbb871a35a5bb69f208c25d56c322189
Sagar Admane <sagaradmane6@gmail.com>
2025-01-23 01:52:39 +0530

    commit new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://your-backend-service.onrender.com',
-    }
-  }
-});
+  plugins: [react()],
+})
